b2d2c2db564d727498d5aebc3e0a4aa973fb8f0f
cdev55 <116827864+cdev55@users.noreply.github.com>
2022-12-23 03:10:36 +0530

    Delete ReferalChecker directory

--- ReferalChecker/server/routes/route.js
+++
@@ -1,8 +0,0 @@
-import  express from "express";
-const router=express.Router();
-import {refrenceChecker} from '../controllers/refrence-checker.js'
-
-router.post('/',refrenceChecker)
-
-export default router;
-
